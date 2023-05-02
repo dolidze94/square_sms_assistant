a787f160526d9ee500e4b4419e987506169d32a7
gdolidze <george.dolidze@jpl.nasa.gov>
2023-05-01 17:06:32 -0700

    More stuff

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
 from configs import secret_key
+from messages import messages
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
+
+messages = messages
 
 # Test route
 @app.route('/api/hello')
@@ -11,13 +14,25 @@ def hello():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
 
 @app.route('/entry/', methods=('GET', 'POST'))
-def create():
+def entry():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('home'))
+
     return render_template('entry.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host='0.0.0.0',port=5000)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0',port=5678)
