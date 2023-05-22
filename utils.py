753fa6857e79483e5728942e9c7f9d215cbfa5ab
gdolidze <george.dolidze@jpl.nasa.gov>
2023-05-21 23:32:15 -0700

    Big fixes

--- utils.py
+++ utils.py
@@ -5,23 +5,21 @@ def create_user(name, phone_number, email):
     # New user in the app
     # Users are ID'd by phone number
 
-    existing_user_data = json_utils.load_data()
+    user_data = json_utils.load_data()
 
-    for user, data in existing_user_data.items():
+    for user, data in user_data.items():
         if user == phone_number:
             error = 'User (%s) already exists' % user
             return error
     
-    new_user_record = {}
-    new_user_record[phone_number] = {
+
+    user_data[phone_number] = {
         'name': name,
         'email': email
     }
 
-    new_user_data = existing_user_data.append(new_user_record)
-
-    json_utils.save_data(new_user_data)
-    return new_user_data
+    json_utils.save_data(user_data)
+    return user_data
 
 def create_customer(name, phone_number, email):
     # Square customer creation
