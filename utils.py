40ebf208a0becc2ea1a43224ee6f61173dbe9db2
gdolidze <george.dolidze@jpl.nasa.gov>
2023-05-27 23:01:13 -0700

    Formatting

--- utils.py
+++ utils.py
@@ -60,7 +60,7 @@ def incoming_processor(data):
         if 'hello' in command:
             response = 'Hello! This is your Square SMS Assistant.\n\nReply "assist" at any time to see how I can help you today.'
         elif 'assist' in command:
-            response = 'Available commands:\n- %s\n\nYou may use the above commands paired with the below Square merchant information:\n%s\nExample:\n List customers' % (available_commands, available_objs)
+            response = "Available commands:\n- %s\n\nYou may use the above commands paired with the below Square merchant information:\n%s\nExample: 'List customers'" % (available_commands, available_objs)
         else:
             response = "Sorry, I don't understand what you mean."
     else:
