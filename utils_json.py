45b6443e5e5d4f68d597a1e1eb9057bc27c76bb6
gdolidze <george.dolidze@jpl.nasa.gov>
2023-05-27 21:47:59 -0700

    Bug fixes

--- utils_json.py
+++ utils_json.py
@@ -28,9 +28,8 @@ def add_user_incoming_history(incoming):
         database = load_data()
         for cust_id, cust_data in database.items():
             if incoming_user == cust_id:
-                cust_data["incoming_history"].append(
-                    (len(cust_data["incoming_history"]), incoming) # Indexed tuple
-                    )
+                entry_tuple = (len(cust_data["incoming_history"]), str(incoming)) # Indexed tuple
+                cust_data["incoming_history"].append(entry_tuple)
                 save_data(database)
         return True
     else:
