be2a32f45700a614820b1b6d97dcfddbb87c4f2f
gdolidze <george.dolidze@jpl.nasa.gov>
2023-05-27 17:25:01 -0700

    Bug fix

--- utils_json.py
+++ utils_json.py
@@ -15,39 +15,39 @@ def save_data(data, file_path = database_file):
 def user_exists(incoming):
     database = load_data()
     print('database after retrieving: %s' % database, file=sys.stderr)
-    for cust_id, cust_data in database:
+    for cust_id, cust_data in database.items():
         if incoming['From'] == cust_id:
             return True
     return False
-
-# def add_user_incoming_history(incoming):
-#     print('add_user_incoming_history executed', file=sys.stderr)
-#     if user_exists(incoming):
-#         incoming_user = incoming['From']
-#         database = load_data()
-#         for cust_id, cust_data in database:
-#             if incoming_user == cust_id:
-#                 existing_incoming_history = cust_data["incoming_history"]
-#                 indexed_history_tuple = (len(existing_incoming_history), incoming)
-#                 existing_incoming_history.append(indexed_history_tuple)
-#                 save_data(database)
-#         return True
-#     else:
-#         return "This user does not exist in the records"
 
 def add_user_incoming_history(incoming):
     print('add_user_incoming_history executed', file=sys.stderr)
     if user_exists(incoming):
         incoming_user = incoming['From']
         database = load_data()
-        for cust_id, cust_data in database:
-            print('incoming user: %s' % incoming_user, file=sys.stderr)
+        for cust_id, cust_data in database.items():
             if incoming_user == cust_id:
                 existing_incoming_history = cust_data["incoming_history"]
                 indexed_history_tuple = (len(existing_incoming_history), incoming)
                 existing_incoming_history.append(indexed_history_tuple)
-                print('database before saving: %s' % database, file=sys.stderr)
                 save_data(database)
         return True
     else:
         return "This user does not exist in the records"
+
+#def add_user_incoming_history(incoming):
+#    print('add_user_incoming_history executed', file=sys.stderr)
+#    if user_exists(incoming):
+#        incoming_user = incoming['From']
+#        database = load_data()
+#        for cust_id, cust_data in database.items():
+#            print('incoming user: %s' % incoming_user, file=sys.stderr)
+#            if incoming_user == cust_id:
+#                existing_incoming_history = cust_data["incoming_history"]
+#                indexed_history_tuple = (len(existing_incoming_history), incoming)
+#                existing_incoming_history.append(indexed_history_tuple)
+#                print('database before saving: %s' % database, file=sys.stderr)
+#                save_data(database)
+#        return True
+#    else:
+#        return "This user does not exist in the records"
